45a408714e9b6e83bb90fa12468c06a35a57f4b1
David Christian <http://bugs.rpath.com>
2005-11-10 16:39:13 -0400

    distribute under the MIT License

--- __init__.py
+++ __init__.py
@@ -1,14 +1,13 @@
 #
 # Copyright (c) 2004-2005 rPath, Inc.
 #
-# This program is distributed under the terms of the Common Public License,
-# version 1.0. A copy of this license should have been distributed with this
-# source file in a file called LICENSE. If it is not present, the license
-# is always available at http://www.opensource.org/licenses/cpl.php.
+# This program is distributed under the terms of the MIT License as found 
+# in a file called LICENSE. If it is not present, the license
+# is always available at http://www.opensource.org/licenses/mit-license.php.
 #
 # This program is distributed in the hope that it will be useful, but
 # without any waranty; without even the implied warranty of merchantability
-# or fitness for a particular purpose. See the Common Public License for
-# full details.
+# or fitness for a particular purpose. See the MIT License for full details.
+
 
 from epdb import *
