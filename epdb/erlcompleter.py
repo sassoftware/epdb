c7cfa2f41c22218beb9cee89e70722ab9f649e70
David Christian <http://bugs.rpath.com>
2005-11-11 11:00:11 -0400

    add erlcompleter

---
+++ epdb/erlcompleter.py
@@ -0,0 +1,36 @@
+#
+# Copyright (c) 2004-2005 rPath, Inc.
+#
+# This program is distributed under the terms of the MIT License as found 
+# in a file called LICENSE. If it is not present, the license
+# is always available at http://www.opensource.org/licenses/mit-license.php.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any waranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the MIT License for full details.
+
+import rlcompleter
+
+class ECompleter(rlcompleter.Completer):
+
+    def global_matches(self, text):
+        """Compute matches when text is a simple name.
+
+        Return a list of all keywords, built-in functions and names currently
+        defined in self.namespace that match.
+
+        """
+# lines commented out make tab completion match keywords and builtins,
+# which we don't want 
+#        import keyword
+        matches = []
+        n = len(text)
+        for list in [
+#                   keyword.kwlist,
+#                     __builtin__.__dict__,
+                     self.namespace]:
+            for word in list:
+                if word[:n] == text and word != "__builtins__":
+                    matches.append(word)
+        return matches
+
