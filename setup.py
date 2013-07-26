91480a5347b93a7efa09f0c31a456e120a69ac4c
Elliot Peele <elliot.peele@sas.com>
2013-07-26 14:27:54 -0400

    update setup.py to 0.14

--- setup.py
+++ setup.py
@@ -26,14 +26,14 @@ try:
 except ImportError:
     from distutils.core import setup
 
-Version = "0.12"
+Version = "0.14"
 
 setup(name = "epdb",
       version = Version,
       description = "Enhanced Python Debugger",
       long_description="Adds functionality to the python debugger, including support for remote debugging",
       author = "rPath, Inc.",
-      author_email = "elliot@rpath.com",
+      author_email = "elliot.peele@sas.com",
       url = "http://bitbucket.org/rpathsync/epdb/",
       packages = [ 'epdb' ],
       license = 'MIT',
